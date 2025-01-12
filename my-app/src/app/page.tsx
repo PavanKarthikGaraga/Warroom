e13790f4cc119d0a06875ed97b68d99581f4a782
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2025-01-12 11:51:04 +0530

    added next app

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+        <div className="home-component">
+          <div className="home-component-in">
+              <h1>WarRoom</h1>
+              <p>By Koneru Lakshmaiah Education Foundation</p>
+          </div>
+        </div>
+  );
+}
